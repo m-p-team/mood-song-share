4d96050926336b8ade788fc1463210ed07c15bf3
git-private-tanaka <konohata0815@gmail.com>
2026-01-28 02:34:26 +0900

    add: users update policy

--- app/lib/userService.ts
+++ app/lib/userService.ts
@@ -1,14 +1,21 @@
-import { supabase } from "./supabaseClient";
+import { supabase } from "@/app/lib/supabaseClient";
 import type { User } from "@supabase/supabase-js";
 
 export async function syncUserToPublicTable(user: User) {
-  const { error } = await supabase.from("users").upsert({
+  const { error, data } = await supabase.from("users").upsert({
     id: user.id,
     name: user.user_metadata.full_name ?? "No Name",
     email: user.email,
   });
 
   if (error) {
-    console.error("Failed to sync user:", error);
+    console.error("Failed to sync user:", {
+      message: error.message,
+      details: error.details,
+      hint: error.hint,
+      code: error.code,
+    });
+  } else {
+    console.log("User synced:", data);
   }
 }
